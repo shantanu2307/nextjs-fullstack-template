c83dee7e3a3dd1bc8cc7d3b0a145500ad41238b3
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-07-23 11:41:01 +0530

    feat: add base template component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,13 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Sample text',
+};
+
+const alt: IBaseTemplate = {
+  sampleTextProp: 'Sample alt text',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
